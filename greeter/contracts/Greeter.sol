dddd8bdfa13636cfecfc0b85d30efe1b2ff1b5e3
tesujiro <tesujiro@gmail.com>
2021-02-03 21:31:28 +0900

    add ownership

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -1,13 +1,34 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
+//import "openzeppelin-solidity/contracts/access/Ownable.sol";
+//contract Greeter is Ownable {
+
 contract Greeter {
-	string private _greeting = "Hello, World!";
+    string private _greeting = "Hello, World!";
+    address private _owner;
 
-	function greet() external view returns(string memory) {
-		return _greeting;
-	}
+    constructor() public {
+	_owner = msg.sender;
+    }
 
-	function setGreeting(string calldata greeting) external {
-		_greeting = greeting;
-	}
+    modifier onlyOwner() {
+	require(
+	    msg.sender == _owner,
+	    "Ownable: caller is not the owner"
+	);
+	_;
+    }
+
+    function greet() external view returns(string memory) {
+	return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+	_greeting = greeting;
+    }
+
+    function owner() public view returns(address) {
+	return _owner;
+    }
+
 }
