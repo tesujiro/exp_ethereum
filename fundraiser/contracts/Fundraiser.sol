f87c11cbd0e7a27245de46f694fdbda13b68ac1c
tesujiro <tesujiro@gmail.com>
2021-02-12 06:10:42 +0900

    add beneficiary and owner

--- fundraiser/contracts/Fundraiser.sol
+++ fundraiser/contracts/Fundraiser.sol
@@ -1,11 +1,37 @@
 pragma solidity >0.4.23 <0.7.0;
 
-contract Fundraiser{
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract Fundraiser is Ownable {
 
     string public name;
+    string public url;
+    string public imageURL;
+    string public description;
 
-    constructor(string memory _name) public {
+    address payable public beneficiary;
+    address public custodian;
+
+    constructor(
+	string memory _name,
+	string memory _url,
+	string memory _imageURL,
+	string memory _description,
+    	address payable _beneficiary,
+	address _custodian
+    )
+    	public
+    {
 	name = _name;
+	url = _url;
+	imageURL = _imageURL;
+	description = _description;
+	beneficiary = _beneficiary;
+	custodian = _custodian;
+	_transferOwnership(_custodian);
     }
 
+    function setBeneficiary( address payable _beneficiary) public onlyOwner {
+	beneficiary = _beneficiary;
+    }
 }
--- fundraiser/test/fundraiser_test.js
+++ fundraiser/test/fundraiser_test.js
@@ -2,16 +2,74 @@ const FundraiserContract = artifacts.require("Fundraiser");
 
 contract("Fundraiser", accounts => {
     let fundraiser;
-    const name = "Beneficiary Name"
+    const name = "Beneficiary Name";
+    const url = "beneficiaryname.org";
+    const imageURL = "https://placekitten.com/600/350";
+    const description = "Beneficiary description";
+    const beneficiary = accounts[1];
+    const owner = accounts[0];
+
+    beforeEach (async() => {
+	fundraiser = await FundraiserContract.new(
+	    name,
+	    url,
+	    imageURL,
+	    description,
+	    beneficiary,
+	    owner
+	);
+    });
 
     describe("initialization", () => {
-	beforeEach (async() => {
-	    fundraiser = await FundraiserContract.new(name);
-	});
-
 	it("gets the beneficiary name", async () => {
 	    const actual = await fundraiser.name();
 	    assert.equal(actual, name, "name should match");
+	});
+
+	it("gets the beneficiary url", async () => {
+	    const actual = await fundraiser.url();
+	    assert.equal(actual, url, "url should match");
+	});
+
+	it("gets the beneficiary image url", async () => {
+	    const actual = await fundraiser.imageURL();
+	    assert.equal(actual, imageURL, "imageURL should match");
+	});
+
+	it("gets the beneficiary description", async () => {
+	    const actual = await fundraiser.description();
+	    assert.equal(actual, description, "description should match");
+	});
+
+	it("gets the beneficiary", async () => {
+	    const actual = await fundraiser.beneficiary();
+	    assert.equal(actual, beneficiary, "beneficiary address should match");
+	});
+
+	it("gets the owner", async () => {
+	    const actual = await fundraiser.owner();
+	    assert.equal(actual, owner, "bios should match");
+	});
+    });
+
+    describe("setBeneficiary", () => {
+	const newBeneficiary = accounts[2];
+
+	it("updatedated beneficiary when called by owner account", async () => {
+	    await fundraiser.setBeneficiary(newBeneficiary, {from: owner});
+	    const actualBeneficiary = await fundraiser.beneficiary();
+	    assert.equal(actualBeneficiary, newBeneficiary, "beneficiaries should match");
+	});
+
+	it("throws an error when called from a non-owner account", async () => {
+	    try {
+		await fundraiser.setBeneficiary(newBeneficiary, {from: accounts[3]});
+		assert.fail("withdraw was not restricted owners");
+	    } catch(err) {
+		const expectedError = "Ownable: caller is not the owner"
+		const actualError = err.reason;
+		assert.equal(actualError, expectedError, "should not be permitted");
+	    }
 	});
     });
 });
