4856e2c47be2ee8222ebb7810378c4de8065433e
tesujiro <tesujiro@gmail.com>
2021-02-14 20:35:01 +0900

    finished hands-on smart contract development with solidity and ethereum chapter9

---
+++ greeter-dapp/contracts/Greeter.sol
@@ -0,0 +1,40 @@
+pragma solidity >= 0.4.0 < 0.7.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+    address private _owner;
+
+    event SetGreeting();
+
+    constructor() public {
+	_owner = msg.sender;
+    }
+
+    /*
+    modifier onlyOwner() {
+	require(
+	    msg.sender == _owner,
+	    "Ownable: caller is not the owner"
+	);
+	_;
+    }
+   */
+
+    function greet() external view returns(string memory) {
+	return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+	_greeting = greeting;
+	emit SetGreeting();
+    }
+
+    /*
+    function owner() public view returns(address) {
+	return _owner;
+    }
+   */
+
+}
