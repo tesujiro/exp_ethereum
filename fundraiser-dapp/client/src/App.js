eb16d60dfc751746c9c6c8e58c2ce808014f8566
tesujiro <tesujiro@gmail.com>
2021-02-15 20:40:41 +0900

    navigation and routes set up

--- fundraiser-dapp/client/src/App.js
+++ fundraiser-dapp/client/src/App.js
@@ -1,76 +1,77 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState, useEffect } from "react";
+import FactoryContract from "./contracts/FundraiserFactory.json";
 import getWeb3 from "./getWeb3";
-
 import "./App.css";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-class App extends Component {
-    state = { storageValue: 0, web3: null, accounts: null, contract: null };
+import NewFundraiser from './NewFundraiser';
+import Home from './Home';
 
-    componentDidMount = async () => {
-	try {
-	    // Get network provider and web3 instance.
-	    const web3 = await getWeb3();
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-	    // Use web3 to get the user's accounts.
-	    const accounts = await web3.eth.getAccounts();
-	    console.log("accounts: " + accounts)
+const App = () => {
+    const [state, setState] =
+	useState({web3: null, accounts: null, contract: null});
+    const [storageValue, setStorageValue] = useState(0);
 
+    useEffect(() => {
+	const init = async() => {
+	    try {
+		const web3 = await getWeb3();
+		const accounts = await web3.eth.getAccounts();
+		const networkId = await web3.eth.net.getId();
+		const deployedNetwork = FactoryContract.networks[networkId];
+		const instance = new web3.eth.Contract(
+		    FactoryContract.abi,
+		    deployedNetwork && deployedNetwork.address,
+		);
 
-	    // Get the contract instance.
-	    const networkId = await web3.eth.net.getId();
-	    console.log("networkId: " + networkId);
-	    const deployedNetwork = SimpleStorageContract.networks[networkId];
-	    const instance = new web3.eth.Contract(
-		SimpleStorageContract.abi,
-		deployedNetwork && deployedNetwork.address,
-	    );
-
-	    // Set web3, accounts, and contract to the state, and then proceed with an
-	    // example of interacting with the contract's methods.
-	    this.setState({ web3, accounts, contract: instance }, this.runExample);
-	} catch (error) {
-	    // Catch any errors for any of the above operations.
-	    alert(
-		`Failed to load web3, accounts, or contract. Check console for details.`,
-	    );
-	    console.error(error);
+		setState({web3, accounts, contract: instance});
+	    } catch(error) {
+		alert(
+		    `Failed to load web3, accounts, or contract.
+	  Check console for details.`,
+		)
+		console.error(error);
+	    }
 	}
+	init();
+    }, []);
+
+    const useStyles = makeStyles({
+	root: {
+	    flexGrow: 1,
+	},
+    });
+
+    const classes = useStyles();
+
+
+    const runExample = async () => {
+	const { accounts, contract } = state;
+
     };
 
-    runExample = async () => {
-	const { accounts, contract } = this.state;
+    return(
+	<Router>
+	  <div>
+	    <AppBar position="static" color="default">
+	      <Toolbar>
+	       <Typography variant="h6" color="inherit">
+		 <NavLink className="nav-link" to="/">Home</NavLink>
+	       </Typography>
+	       <NavLink className="nav-link" to="/new/">New Fundraiser</NavLink>
+	      </Toolbar>
+	   </AppBar>
 
-	// Stores a given value, 5 by default.
-	await contract.methods.set(5).send({ from: accounts[0] });
-
-	// Get the value from the contract to prove it worked.
-	const response = await contract.methods.get().call();
-
-	// Update state with the result.
-	this.setState({ storageValue: response });
-    };
-
-    render() {
-	if (!this.state.web3) {
-	    return <div>Loading Web3, accounts, and contract...</div>;
-	}
-	return (
-	    <div className="App">
-	    <h1>Good to Go!</h1>
-	    <p>Your Truffle Box is installed and ready.</p>
-	    <h2>Smart Contract Example</h2>
-	    <p>
-	    If your contracts compiled and migrated successfully, below will show
-	    a stored value of 5 (by default).
-	    </p>
-	    <p>
-	    Try changing the value stored on <strong>line 40</strong> of App.js.
-	    </p>
-	    <div>The stored value is: {this.state.storageValue}</div>
-	    </div>
-	);
-    }
+	    <Route path="/" exact component={Home} />
+	    <Route path="/new/" component={NewFundraiser} />
+	  </div>
+	</Router>
+    );
 }
 
 export default App;
